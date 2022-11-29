e9ae3a1c82f0a5f4c81db754cf8f4ee28afe1581
prajwal-np <prajwaldeepbhandari@gmail.com>
2022-11-29 01:40:04 -0500

    linux testing and fixing

--- src/main/java/com/kiosk/kioskVerifone/service/CommerceListener.java
+++ src/main/java/com/kiosk/kioskVerifone/service/CommerceListener.java
@@ -1,26 +1,125 @@
 package com.kiosk.kioskVerifone.service;
 
-import com.verifone.payment_sdk.CommerceListenerAdapter;
-import com.verifone.payment_sdk.PaymentCompletedEvent;
-import com.verifone.payment_sdk.Status;
-import com.verifone.payment_sdk.TransactionEvent;
+import com.verifone.payment_sdk.*;
+
+import java.util.logging.Logger;
 
 class CommerceListener {
-    private final CommerceListenerAdapter mCommerceListener = new CommerceListenerAdapter() {
+    private static final String TAG = "CommerceListener";
+    private final static Logger LOGGER = Logger.getLogger(PaymentSdk.class.getName());
+    public  CommerceListener2 mCommerceListener = new CommerceListener2() {
         @Override
         public void handleStatus(Status status){
-            System.out.println(status);
+            String s = status.getMessage() +":"+status.getStatus();
+            LOGGER.info(TAG+"::"+s);
+        }
+
+        @Override
+        public void handleDeviceVitalsInformationEvent(DeviceVitalsInformationEvent deviceVitalsInformationEvent) {
+
+        }
+
+        @Override
+        public void handlePinEvent(PinEvent pinEvent) {
+
+        }
+
+        @Override
+        public void handleCommerceEvent(CommerceEvent commerceEvent) {
+
         }
 
         @Override
         public  void handleTransactionEvent(TransactionEvent event){
-            System.out.println(event);
+        }
+
+        @Override
+        public void handleAmountAdjustedEvent(AmountAdjustedEvent amountAdjustedEvent) {
+
+        }
+
+        @Override
+        public void handleBasketAdjustedEvent(BasketAdjustedEvent basketAdjustedEvent) {
+
+        }
+
+        @Override
+        public void handleBasketEvent(BasketEvent basketEvent) {
+
+        }
+
+        @Override
+        public void handleCardInformationReceivedEvent(CardInformationReceivedEvent cardInformationReceivedEvent) {
+
+        }
+
+        @Override
+        public void handleDeviceManagementEvent(DeviceManagementEvent deviceManagementEvent) {
+
+        }
+
+        @Override
+        public void handleLoyaltyReceivedEvent(LoyaltyReceivedEvent loyaltyReceivedEvent) {
+
         }
 
         @Override
         public void handlePaymentCompletedEvent(PaymentCompletedEvent paymentCompletedEvent) {
             String s = "handlePaymentCompletedEvent: " + paymentCompletedEvent.getType() + ":" + paymentCompletedEvent.getMessage();
+            LOGGER.info(TAG+"::"+s);
         }
+
+        @Override
+        public void handleReceiptDeliveryMethodEvent(ReceiptDeliveryMethodEvent receiptDeliveryMethodEvent) {
+
+        }
+
+        @Override
+        public void handleStoredValueCardEvent(StoredValueCardEvent storedValueCardEvent) {
+
+        }
+
+        @Override
+        public void handleUserInputEvent(UserInputEvent userInputEvent) {
+
+        }
+
+        @Override
+        public void handleReconciliationEvent(ReconciliationEvent reconciliationEvent) {
+
+        }
+
+        @Override
+        public void handleReconciliationsListEvent(ReconciliationsListEvent reconciliationsListEvent) {
+
+        }
+
+        @Override
+        public void handleTransactionQueryEvent(TransactionQueryEvent transactionQueryEvent) {
+
+        }
+
+        @Override
+        public void handleHostAuthorizationEvent(HostAuthorizationEvent hostAuthorizationEvent) {
+
+        }
+
+        @Override
+        public void handleHostFinalizeTransactionEvent(HostFinalizeTransactionEvent hostFinalizeTransactionEvent) {
+
+        }
+
+        @Override
+        public void handleNotificationEvent(NotificationEvent notificationEvent) {
+
+        }
+
+        @Override
+        public void handlePrintEvent(PrintEvent printEvent) {
+
+        }
+
     };
+
 
 }
--- src/main/java/com/kiosk/kioskVerifone/service/SaleService.java
+++ src/main/java/com/kiosk/kioskVerifone/service/SaleService.java
@@ -9,6 +9,7 @@ import com.verifone.payment_sdk.AmountTotals;
 import com.verifone.payment_sdk.Payment;
 import com.verifone.payment_sdk.TransactionManager;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 
@@ -16,36 +17,61 @@ import java.util.logging.Logger;
 @Service
 public class SaleService extends CommerceListener{
 
-    private static final String TAG = "PaymentSdkSaleHandler";
+    private static final String TAG = "SalesSrvice";
     final PaymentDao paymentDao;
     private PaymentSdk paymentSdk;
     private Payment payment;
+
+    private String connectionType= "tcpip";
+    private String terminalAddress="127.0.0.1";
 
     private final static Logger LOGGER = Logger.getLogger(PaymentSdk.class.getName());
 
     @Autowired
     public SaleService(PaymentDao paymentDao){
         this.paymentDao = paymentDao;
+        paymentSdk = PaymentSdk.create();
+        initSdk();
+        startSession();
+        PaymentSdk.configureLogFile("logfile.text",1024*5);
+    }
+
+    public void initSdk(){
+        HashMap<String,String> paramMap = new HashMap<>();
+        paramMap.put(PsdkDeviceInformation.DEVICE_CONNECTION_TYPE_KEY, connectionType);
+        paramMap.put(PsdkDeviceInformation.DEVICE_SERIAL_NUMBER_KEY, PsdkDeviceInformation.ACCEPT_ANY_DEVICE_VALUE);
+        paramMap.put(PsdkDeviceInformation.DEVICE_ADDRESS_KEY, terminalAddress);
+        paymentSdk.initialize(mCommerceListener);
+    }
+
+    public void startSession(){
+        Transaction transaction = Transaction.create();
+        transaction.setCurrency("EUR");
+        boolean result = paymentSdk.getTransactionManager().startSession2(transaction);
+        LOGGER.info(TAG+"::"+result);
+    }
+
+    public void logoutSession(){
+        boolean result = paymentSdk.getTransactionManager().endSession();
+        String s = "startSession returned" + result;
+        LOGGER.info(TAG+"::"+s);
     }
     public String initiatePayment(PaymentModel paymentList){
        try{
            this.paymentDao.VerifonePayment(paymentList);
-           paymentSdk = PaymentSdk.create();
            payment = Payment.create();
 
            // set payment amount start
            AmountTotals requestedAmounts = payment.getRequestedAmounts();
-           System.out.println("Tax"+this.paymentDao.getTax());
-           requestedAmounts.setTax(this.paymentDao.getTax());
-           requestedAmounts.setTotal(this.paymentDao.getAmount());
-           System.out.println(this.paymentDao.getTax());
+//           requestedAmounts.setTax(this.paymentDao.getTax());
+           requestedAmounts.setTotal(new Decimal(10));
+           payment.setRequestedAmounts(requestedAmounts);
            // end
 
            TransactionManager transactionManager = paymentSdk.getTransactionManager();
            Status status = transactionManager.startPayment(payment);
-           LOGGER.info(status.getMessage());
-
-           return "Payment Initiated";
+           LOGGER.info("payment"+"::" + status.getMessage());
+           return   String.valueOf(status.getStatus());
        } catch(Error e){
            LOGGER.info(e.getLocalizedMessage());
            return "Payment failed";
